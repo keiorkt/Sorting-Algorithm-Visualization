baf667951ac1df992375c019541eb0340003ac30
Keita Iwasaki <yfu.keita.america@gmail.com>
2019-11-24 10:47:44 +0900

    complete paintwidget.h and paintwidget.cpp files

---
+++ sorting-algorithm-visualization/paintwidget.cpp
@@ -0,0 +1,56 @@
+#include "paintwidget.h"
+
+PaintWidget::PaintWidget(QWidget *parent) : QWidget(parent) {
+    palette.setColor(QPalette::Background, backgroundColor);
+    setPalette(palette);
+    setAutoFillBackground(true);
+    animate = false;
+    penWidth =5;
+    spacing = 0;
+
+    setPen(QPen(lineColor, penWidth, Qt::PenStyle::SolidLine, Qt::PenCapStyle::SquareCap, Qt::PenJoinStyle::BevelJoin));
+}
+
+void PaintWidget::setPaintData(std::vector<int> n, std::vector<int> idx) {
+    mutex.lock();
+
+    numbers = n;
+    indices = idx;
+
+    mutex.unlock();
+}
+
+void PaintWidget::paintEvent(QPaintEvent *) {
+    QPainter painter(this);
+
+    int iColor{0};
+
+    for (int i{0}; i < numbers.size(); ++i) {
+        pen.setColor(lineColor);
+        painter.setPen(pen);
+
+        bool contain{false};
+        for (int x : indices) {
+            if (x == i) {
+                contain = true;
+            }
+        }
+
+        if (animate && contain) {
+            pen.setColor(colorList[iColor]);
+            painter.setPen(pen);
+            ++iColor;
+        }
+
+        painter.drawLine((i-1)*penWidth, this->height(), (i-1)*penWidth, this->height() - numbers[i]);
+    }
+}
+
+void PaintWidget::onNumberSizeChange(int size) {
+    penWidth = this->width() / size - spacing;
+    this->update();
+}
+
+void PaintWidget::setBrush(const QBrush &brush) {
+    this->brush = brush;
+}
---
+++ sorting-algorithm-visualization/paintwidget.h
@@ -0,0 +1,53 @@
+#ifndef PAINTWIDGET_H
+#define PAINTWIDGET_H
+
+#include <QObject>
+#include <QtWidgets>
+#include <QMutex>
+#include <QPainter>
+#include <list>
+#include <vector>
+
+
+const QColor defaultLineColor = Qt::lightGray;
+const QColor defaultBgColor = Qt::black;
+
+class PaintWidget : public QWidget {
+    Q_OBJECT
+public:
+    explicit PaintWidget(QWidget *parent = nullptr);
+    void setLineColor(QColor color) { lineColor = color;}
+    void setAnim(bool isAnim) { animate = isAnim;}
+    void setPaintData(std::vector<int>, std::vector<int>);
+    void setPenWidth(int w) { penWidth = w;}
+    void setSpacing(int s) { spacing = s;}
+    void resetColor() {lineColor = defaultLineColor;}
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+public slots:
+    void onNumberSizeChange(int size);
+
+private:
+    QPen pen;
+    QBrush brush;
+    QPalette palette;
+
+    QColor lineColor {defaultLineColor};
+    QColor backgroundColor {defaultBgColor};
+    std::vector<QColor> colorList = {Qt::red, Qt::green, Qt::cyan, Qt::magenta, Qt::yellow, Qt::white};
+
+    bool animate;
+    int penWidth;
+    int spacing;
+
+    std::vector<int> numbers;
+    std::vector<int> indices;
+
+    void setPen(const QPen &pen);
+    void setBrush(const QBrush &brush);
+    QMutex mutex;
+};
+
+#endif // PAINTWIDGET_H
