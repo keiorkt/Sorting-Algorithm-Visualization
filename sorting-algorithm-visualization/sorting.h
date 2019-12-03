38b500e7995b60a1f19f5b19aed01a2e9ae71441
ykleeac <54428643+ykleeac@users.noreply.github.com>
2019-12-03 23:01:14 +0900

    Update sorting.h

--- sorting-algorithm-visualization/sorting.h
+++ sorting-algorithm-visualization/sorting.h
@@ -20,7 +20,7 @@ private:
     QObject* parent;
     QString algorithm;
     QString shuffleType;
-    QStringList algorithms = {"Bubble Sort","Insertion Sort","Selection Sort","Quicksort","Cocktail Sort","Merge Sort","Heap Sort","Gnome Sort","Counting Sort"};
+    QStringList algorithms = {"Bubble Sort","Insertion Sort","Selection Sort","Quicksort","Cocktail Sort","Merge Sort","Heap Sort","Gnome Sort","Counting Sort","Radix Sort (Base 2)","Radix Sort (Base 10)"};
     QStringList shuffles = {"Random", "Reverse", "Almost sorted"};
     QStringList sizelist = {"2","4","8","16","32","64","128","256","512","1024"};
     int size = DEFAULT_SIZE;
@@ -67,7 +67,8 @@ public:
     void sort_merge(int* arr,int size,int start,int end);
     void sort_heap(int* arr,int size);
     void sort_gnome(int* arr,int size);
-    void sort_counting(int* arr,int size);
+    void sort_counting(int* arr,int size,int base,int exp);
+    void sort_radix(int*arr,int size,int base);
 
     void visualize(int* arr,int size_arr,int* colorindex,int size_color);
 
